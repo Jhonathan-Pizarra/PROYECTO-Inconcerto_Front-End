d16bbe3da9316db6e60bdae61c1df139d6fc1146
Jhonathan-Pizarra <jhonnathan.pizarra@epn.edu.ec>
2021-04-18 13:29:04 -0500

    1-proyectoBase

---
+++ src/pages/festivales/index.js
@@ -0,0 +1,11 @@
+import Navigation from "../../components/Navigation";
+
+export default function Festivales() {
+    return(
+        <div>
+            <Navigation/>
+            <p>Festivales!!</p>
+        </div>
+
+    )
+}
