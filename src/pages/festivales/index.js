289c21f6cd03652fc2562914fab86d59dd7057b5
Jhonathan-Pizarra <jhonnathan.pizarra@epn.edu.ec>
2021-04-19 01:33:36 -0500

    proyectoBase

--- src/pages/festivales/index.js
+++ src/pages/festivales/index.js
@@ -1,11 +1,37 @@
 import Navigation from "../../components/Navigation";
 
-export default function Festivales() {
+export default function Festivales({festivals}) {
     return(
         <div>
             <Navigation/>
-            <p>Festivales!!</p>
+            <h1>Festivales InConcerto</h1>
+            <div>
+                {festivals.data.map(festival => {
+                    return(
+                        <div key={festival.id}>
+                            <a>
+                                <p>Name: {festival.name}</p>
+                                <p>Description: {festival.description}</p>
+                                <br/>
+                            </a>
+                        </div>
+                    )
+                })}
+            </div>
+
         </div>
 
     )
+}
+
+//Static Render Content
+export async function getStaticProps() {
+    const resp = await fetch('http://localhost:8000/api/festivals');
+    const festivals = await resp.json();
+
+    return {
+        props:{
+            festivals
+        }
+    }
 }
